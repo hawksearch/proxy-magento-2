3b5b3c819a3ba76ac6a69e8cefe65b326efec690
Andrew Stayart <andrew.stayart@americaneagle.com>
2016-10-05 15:06:08 -0500

    removing fulltext search filtering

---
+++ Model/ResourceModel/Collection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mageuser
+ * Date: 10/4/16
+ * Time: 3:44 PM
+ */
+
+namespace HawkSearch\Proxy\Model\ResourceModel;
+
+
+class Collection
+extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection
+{
+    protected function _renderFiltersBefore()
+    {
+        return;
+    }
+
+}
