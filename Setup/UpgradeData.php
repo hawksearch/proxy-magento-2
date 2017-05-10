dfd7154e425c8fe4332a26d913d5cc94e41cd060
AE\martin.blaszczyk <martin.blaszczyk@americaneagle.com>
2017-05-10 17:21:10 -0500

    updating the atribute->save to updateAttribute.

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -40,7 +40,7 @@ class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
         $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
         $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Category::ENTITY);
         $attributeSetId = $categorySetup->getDefaultAttributeSetId($entityTypeId);
-        if (version_compare($context->getVersion(), '2.1.1', '<')) {
+        if (version_compare($context->getVersion(), '2.1.2', '<')) {
             $attribute = $categorySetup->getAttribute(\Magento\Catalog\Model\Category::ENTITY, 'hawk_landing_page');
             if($attribute){
 				$idg = $categorySetup->getAttributeGroupId($entityTypeId, $attributeSetId, 'Display Settings');
@@ -51,6 +51,16 @@ class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
 				'hawk_landing_page',
 				46
 			);
+			$categorySetup->updateAttribute(
+				\Magento\Catalog\Model\Category::ENTITY,
+				'group',
+				'hawk_landing_page',
+				'Display Settings'
+			);
+				
+				
+			// $attribute->setData('group', 'Display Settings')->save();
+			
                 
             }
         }
