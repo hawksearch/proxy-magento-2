ee22c5bae9ed5c32ffbda948e29ad336b77063a7
Andrew Stayart <andrew.stayart@americaneagle.com>
2018-10-29 12:19:45 -0500

    army navy fork with filtering hotfix

--- Model/ResourceModel/Collection.php
+++ Model/ResourceModel/Collection.php
@@ -14,7 +14,7 @@ extends \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection
 {
     protected function _renderFiltersBefore()
     {
-        return;
+        return parent::_renderFiltersBefore();
     }
 
 }
