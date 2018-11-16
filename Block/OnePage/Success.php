15f9f5faba0c66ecf6cb825f264c716e712024b5
Andrew Stayart <andrew.stayart@americaneagle.com>
2018-11-16 13:43:09 -0600

    moving tracking pixel to additional info block

---
+++ Block/OnePage/Success.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 11/16/18
+ * Time: 9:55 AM
+ */
+
+namespace HawkSearch\Proxy\Block\OnePage;
+
+
+class Success extends \Magento\Checkout\Block\Onepage\Success
+{
+    /**
+     * @var \HawkSearch\Proxy\Helper\Data
+     */
+    private $helper;
+    /**
+     * @var \Magento\Catalog\Model\Session
+     */
+    private $session;
+
+    public function __construct(
+        \HawkSearch\Proxy\Helper\Data $helper,
+        \Magento\Catalog\Model\Session $session,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Sales\Model\Order\Config $orderConfig,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = [])
+    {
+        parent::__construct($context, $checkoutSession, $orderConfig, $httpContext, $data);
+        $this->helper = $helper;
+        $this->session = $session;
+    }
+
+    public function getTrackingPixelUrl() {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        return $this->helper->getTrackingPixelUrl([
+            'd' => $this->helper->getOrderTackingKey(),
+            'hawksessionid' => $this->session->getSessionId(),
+            'orderno' => $order->getIncrementId(),
+            'total' => $order->getGrandTotal()
+        ]);
+    }
+}
