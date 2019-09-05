9dd6184adff404a74d5d56bbd75886a6da0c89e8
Andrew Stayart <andrew.stayart@americaneagle.com>
2019-09-05 08:12:02 -0500

    adding ajax based tracking, may need to change course on this

---
+++ Model/Observer/AddToCart.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace HawkSearch\Proxy\Model\Observer;
+
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class AddToCart implements ObserverInterface
+{
+
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        // TODO: Implement execute() method.
+    }
+}
